4da2ecbd42a5f675e1d840429e289d79449ae266
ANS0025 <niko.aegis@gmail.com>
2023-10-02 21:59:09 +0900

    fix: norminette violation

--- get_next_line.c
+++ get_next_line.c
@@ -12,6 +12,32 @@
 
 #include "get_next_line.h"
 
+static char	*join_buffer(char *line, char *buffer, int read_bytes)
+{
+	char	*new_line;
+
+	buffer[read_bytes] = '\0';
+	if (!line)
+		new_line = ft_strdup(buffer);
+	else
+		new_line = ft_strjoin(line, buffer);
+	free(buffer);
+	return (new_line);
+}
+
+static char	*get_line(char *line, int i)
+{
+	char	*new_line;
+
+	if (line[i] == '\n')
+		new_line = ft_substr(line, 0, i + 1);
+	else if (line[i] == '\0' && i > 0)
+		new_line = ft_substr(line, 0, i);
+	else
+		new_line = NULL;
+	return (new_line);
+}
+
 char	*get_next_line(int fd)
 {
 	static char	*line;
@@ -26,21 +52,10 @@ char	*get_next_line(int fd)
 		return (NULL);
 	read_bytes = read(fd, buffer, BUFFER_SIZE);
 	if (read_bytes == -1)
-	{
-		free(buffer);
-		return (NULL);
-	}
-	buffer[read_bytes] = '\0';
-	if (!line)
-		line = ft_strdup(buffer);
-	else
-		line = ft_strjoin(line, buffer);
-	free(buffer);
+		return (free(buffer), NULL);
+	line = join_buffer(line, buffer, read_bytes);
 	i = 0;
 	while (line[i] != '\n' && line[i] != '\0')
 		i++;
-	if (line[i] == '\n')
-		return (ft_substr(line, 0, i + 1));
-	else
-		return (NULL);
+	return (get_line(line, i));
 }
